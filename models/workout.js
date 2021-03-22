5d927700474e15f0b729a9bb16a4695742581ce7
pfotis <69961508+pfotis@users.noreply.github.com>
2021-03-23 09:05:53 +1100

    create and complete the workout.js

---
+++ models/workout.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Enter a type of exercise"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter a name of exercise"
+            },
+            duration: {
+                type: Number,
+                required: "Enter an length"
+            },
+            weight: {
+                type: Number,
+                required: "Enter an weight"
+            },
+            reps: {
+                type: Number,
+                required: "Enter the reps"
+            },
+            sets: {
+                type: Number,
+                required: "Enter the sets"
+            }
+        }
+    ]
+});
+
+const Transaction = mongoose.model("Transaction", transactionSchema);
+
+module.exports = Transaction;
